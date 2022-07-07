55de10227b5ed864926e22b416a258680fd0eb00
Suleiman Olaide <olaide.suleiman96@gmail.com>
2022-07-07 08:17:40 -0700

    Use class components

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,32 +1,39 @@
-const Calculator = () => (
-  <div className="calculator-wrapper">
+import React from 'react';
 
-    <div className="calculator">
-      <div className="output">
-        <span>0</span>
+class Calculator extends React.PureComponent {
+  render() {
+    return (
+      <div className="calculator-wrapper">
+
+        <div className="calculator">
+          <div className="output">
+            <span>0</span>
+          </div>
+          <div className="operators-and-digits">
+            <button type="button">AC</button>
+            <button type="button">+/-</button>
+            <button type="button">%</button>
+            <button type="button" className="colored">÷</button>
+            <button type="button">7</button>
+            <button type="button">8</button>
+            <button type="button">9</button>
+            <button type="button" className="colored">x</button>
+            <button type="button">4</button>
+            <button type="button">5</button>
+            <button type="button">6</button>
+            <button type="button" className="colored">-</button>
+            <button type="button">1</button>
+            <button type="button">2</button>
+            <button type="button">3</button>
+            <button type="button" className="colored">+</button>
+            <button type="button" className="wide">0</button>
+            <button type="button">.</button>
+            <button type="button" className="colored">=</button>
+          </div>
+        </div>
       </div>
-      <div className="operators-and-digits">
-        <button type="button">AC</button>
-        <button type="button">+/-</button>
-        <button type="button">%</button>
-        <button type="button" className="colored">÷</button>
-        <button type="button">7</button>
-        <button type="button">8</button>
-        <button type="button">9</button>
-        <button type="button" className="colored">x</button>
-        <button type="button">4</button>
-        <button type="button">5</button>
-        <button type="button">6</button>
-        <button type="button" className="colored">-</button>
-        <button type="button">1</button>
-        <button type="button">2</button>
-        <button type="button">3</button>
-        <button type="button" className="colored">+</button>
-        <button type="button" className="wide">0</button>
-        <button type="button">.</button>
-        <button type="button" className="colored">=</button>
-      </div>
-    </div>
-  </div>
-);
+    );
+  }
+}
+
 export default Calculator;
